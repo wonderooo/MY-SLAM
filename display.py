49e240c12004701a24fa853116c5ad64877618c1
kacper szczesny <kacperek.szczesny@gmail.com>
2022-06-20 20:02:29 +0200

    3d mapping something ;), multiprocessing, NEED TO REFACTOR

--- display.py
+++ display.py
@@ -2,6 +2,8 @@ import sdl2.ext
 import sdl2
 import numpy as np
 import sdl2.sdlgfx
+import pypangolin as pango
+from OpenGL.GL import *
 
 class FeaturesPreview(object):
     def __init__(self, width: int, hegiht: int, fps: int) -> None:
@@ -30,3 +32,44 @@ class FeaturesPreview(object):
 
         #delaying frame for good fps
         sdl2.sdlgfx.SDL_framerateDelay(self.frame_manager)
+
+class Map3d(object):
+    def __init__(self, queue):
+        self.width = 1080
+        self.height = 640
+        self.window = pango.CreateWindowAndBind("my_SLAM_mapping", self.width, self.height)
+        glEnable(GL_DEPTH_TEST)
+        self.scam = pango.OpenGlRenderState(
+                    pango.ProjectionMatrix(self.width, self.height, 420, 420, self.width//2, self.height//2, 0.8, 100),
+                    pango.ModelViewLookAt(0, -10, -8,
+                                        0, 0, 0,
+                                        0, -1, 0))
+        handler = pango.Handler3D(self.scam)
+        self.d_cam = pango.CreateDisplay()
+        self.d_cam = self.d_cam.SetBounds(pango.Attach(0),pango.Attach(1),pango.Attach(0),pango.Attach(1), self.width/self.height)
+        self.d_cam = self.d_cam.SetHandler(handler)
+        self.queue = queue
+        self.time = 0.0
+        self.run()
+
+    def add_points(self, points):
+        self.points = points
+
+    def draw_points(self) -> None:
+        color = np.random.random(3)
+        for point in self.queue.get():
+            point_x, point_y, point_z = point[0] / 75, point[1] / 75, 0
+            glColor3d(color[0], color[1], color[2])
+            glVertex3d(point_x, point_y, point_z)
+    
+    def run(self):
+        while not pango.ShouldQuit():
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            self.d_cam.Activate(self.scam)
+            glEnable(GL_POINT_SMOOTH)
+            glPointSize(5)
+            glBegin(GL_POINTS)
+            self.draw_points()
+            glEnd()
+            pango.FinishFrame()
+            self.time += 1
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
-from display import FeaturesPreview
+from display import FeaturesPreview, Map3d
 from frame import Frame
+import multiprocessing as mp
 
-
-if __name__ == '__main__':
+def run(q):
     cap = cv2.VideoCapture("videos/0.hevc")
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -16,6 +16,22 @@ if __name__ == '__main__':
         if ret:
             frame = Frame(np.array(frame))
             kps, des = frame.extract()
+            lordoza = list(map(lambda kp: (kp.pt[0], kp.pt[1]), kps))
+            q.put(lordoza)
             display2d.draw(frame.process(kps))
         else:
             exit(0)
+def run2(q):
+    map3d = Map3d(q)
+processes = []
+
+if __name__ == '__main__':
+    queue = mp.Queue()
+
+    processes.append(mp.Process(target=run, args=(queue,)))
+    processes.append(mp.Process(target=run2, args=(queue,)))
+    for proc in processes:
+        proc.start()
+    for proc in processes:
+        proc.join()
+    
